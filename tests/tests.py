6c383807d7be7d9d1a70f9351294cb5656279368
Alexander L. Hayes <alexander@batflyer.net>
2017-08-15 10:49:05 -0400

    Fixed the travis test with a try/except boostsrl import. Added test for modes.

--- tests/tests.py
+++ tests/tests.py
@@ -3,35 +3,39 @@ import os
 import sys
 import unittest
 
-#sys.path.insert(0, '../../boostsrl/')
-#import boostsrl
-#from boostsrl import boostsrl
+# Path problems tend to be caused depending on which directory tests begin in.
+try:
+    sys.path.insert(0, '../boostsrl/')
+    import boostsrl
+except:
+    sys.path.insert(0, './boostsrl/')
+    import boostsrl
 
-class BasicFunctions:
+class background_functions:
 
-    def build_background(self):
+    def build_background_1(self):
         bk = ['friends(+Person, -Person).', 'friends(-Person, +Person).', 'smokes(+Person).', 'cancer(+Person).']
-        model = boostsrl.model()
+        background = boostsrl.modes(bk, 'cancer', useStdLogicVariables=True, treeDepth=4, nodeSize=2, numOfClauses=8)
+        return background
 
-    def function(self, x):
-        return x + 1
-
-    def function2(self, x):
-        return [1,2,3]
+    def build_background_2(self):
+        bk = ['friends(+Person, -Person).', 'friends(-Person, +Person).', 'smokes(+Person).', 'cancer(+Person).']
 
 class MyTest(unittest.TestCase):
 
-    def test_background_created(self):
-        pass
-
-    def test_assert_true(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-    
-    def test_functions_above(self):
-        f = BasicFunctions()
-        self.assertEqual(f.function(3),4)
-        self.assertEqual(f.function2(5),[1,2,3])
+    def test_background_setup_1(self):
+        f = background_functions()
+        background = f.build_background_1()
+        # These background values should be bound since they are set with the build_background function.
+        self.assertEqual(background.target, 'cancer')
+        self.assertEqual(background.useStdLogicVariables, True)
+        self.assertEqual(background.treeDepth, 4)
+        self.assertEqual(background.nodeSize, 2)
+        self.assertEqual(background.numOfClauses, 8)
+        # These background values should remain as the default values since they are not referenced.
+        self.assertEqual(background.loadAllLibraries, False)
+        self.assertEqual(background.usePrologVariables, False)
+        self.assertEqual(background.maxTreeDepth, None)
 
 if __name__ == '__main__':
     unittest.main()
