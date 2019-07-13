08283d1b1bf8953e05ac4fbd8ac9b63b2430b506
hayesall <alexander@batflyer.net>
2019-07-13 14:06:09 -0400

    🏗️ `boostsrl.base.BaseBoostedRelationalModel` base class
    Add `boostsrl/base.py` base class for deriving estimators
    Add `boostsrl/tests/test_base.py` to test this base class
    Refactor `boostsrl/rdn.py` to use the base class
    
    Several estimators will need to be derived which need access
    to a common set of methods. This base class handles the logic
    for verifying parameters, checking estimators, and leaves
    `fit()`, `predict()` and `predict_proba()` NotImplemented.
    
    This is intended to help prevent code duplication as other
    methods (Markov Logic Networks, RDNRegression, etc.) are
    added.

---
+++ boostsrl/tests/test_base.py
@@ -0,0 +1,43 @@
+# Copyright © 2017, 2018, 2019 Alexander L. Hayes
+
+"""
+Tests for boostsrl.base.BaseBoostedRelationalModel
+"""
+
+import pytest
+from boostsrl.base import BaseBoostedRelationalModel
+
+
+def test_initialize_base_classifier():
+    """Initialize a base classifier with default parameters."""
+    _bm = BaseBoostedRelationalModel()
+    assert _bm.target == "None"
+    assert _bm.n_estimators == 10
+
+
+@pytest.mark.parametrize("test_input", [4, 10, 20])
+def test_initialize_base_classifier_trees(test_input):
+    """Initialize a BaseModel with various tree numbers."""
+    _bm = BaseBoostedRelationalModel(n_estimators=test_input)
+    assert _bm.n_estimators == test_input
+
+
+def test_bad_fit():
+    """Check that fit raises a NotImplementedError"""
+    _bm = BaseBoostedRelationalModel()
+    with pytest.raises(NotImplementedError):
+        _bm.fit("database")
+
+
+def test_bad_predict():
+    """Check that predict raises a NotImplementedError"""
+    _bm = BaseBoostedRelationalModel()
+    with pytest.raises(NotImplementedError):
+        _bm.predict("database")
+
+
+def test_bad_predict_proba():
+    """Check that predict_proba raises a NotImplementedError"""
+    _bm = BaseBoostedRelationalModel()
+    with pytest.raises(NotImplementedError):
+        _bm.predict_proba("database")
